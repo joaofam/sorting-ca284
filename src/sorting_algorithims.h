84ff5a0bd6dd1f19139c036181f8f524153ca634
Joao Pereira <joao.pereira2@mail.dcu.ie>
2021-01-08 03:18:02 +0000

    Modifications to insertion sort

--- src/sorting_algorithims.h
+++ src/sorting_algorithims.h
@@ -2,9 +2,9 @@
 
 void swap(int *x, int *y);
 void selection_sort(int *a, const int n);
-void display(int array[], int size);
+void display(int array[], int n);
 
-void insertion_sort(int *a, const int size);
+void insertion_sort(int *a, int n);
 
 void swap(int *x, int *y)
 {
@@ -14,14 +14,14 @@ void swap(int *x, int *y)
 	*y = tmp;
 }
 
-void selection_sort(int* array, const int size)
+void selection_sort(int* array, const int n)
 {
 	int i, j;
 	int min;
 
-	for (i = 0; i < size - 1; i++){
+	for (i=0; i<n-1; i++){
         min = i;
-        for (j = i + 1; j < size; j++){
+        for (j=i+1; j<n; j++){
             if (array[j] < array[min]){
                 min = j;
             }
@@ -30,14 +30,15 @@ void selection_sort(int* array, const int size)
     }
 }
 
-void insertion_sort(int *array, const int n)
+void insertion_sort(int *array,const int n)
 {
-	int i, j, k;
-	for(i=0;i<n;i++){
-		k = array[j];
-		j = i - 1;
-		while ((j >= 0) && (k < array[j]))
-        {
+ 
+    int i,j, k;
+    for(i=1; i<n; ++i)
+    {
+        k=array[i];
+        j=i-1;
+        while ((j >= 0) && (k < array[j])){
             array[j + 1] = array[j];
             --j;
         }
@@ -46,10 +47,11 @@ void insertion_sort(int *array, const int n)
 }
 
  /* This display function is utilized to print each element of the array */
-void display(int array[], int size)
+
+void display(int array[], int n)
 {
 	int i;
-	for(i=0;i<size;i++){
+	for(i=0;i<n;i++){
 		printf("%d ", array[i]);
 	printf("\n");
 	}
