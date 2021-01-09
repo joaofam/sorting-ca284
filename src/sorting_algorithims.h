d7cd5bfd4a8ef41840722346a4e6a77c487675ed
Joao Pereira <joao.pereira2@mail.dcu.ie>
2021-01-09 19:20:01 +0000

    Radix Sort added to sorting algorithims

--- src/sorting_algorithims.h
+++ src/sorting_algorithims.h
@@ -1,10 +1,14 @@
 /* 3 algorithims */
 
 void swap(int *x, int *y);
-void selection_sort(int *a, const int n);
+void selection_sort(int *array, const int n);
 void display(int array[], int n);
 
 void insertion_sort(int *a, int n);
+
+int getMax(int arr[], int n);
+void countSort(int arr[], int n, int exp);
+void radixsort(int arr[], int n);
 
 void swap(int *x, int *y)
 {
@@ -46,7 +50,56 @@ void insertion_sort(int *array,const int n)
     }
 }
 
- /* This display function is utilized to print each element of the array */
+int getMax(int arr[], int n) 
+{ 
+    int mx = arr[0]; 
+    for (int i = 1; i < n; i++) 
+        if (arr[i] > mx) 
+            mx = arr[i]; 
+    return mx; 
+} 
+  
+// A function to do counting sort of arr[] according to 
+// the digit represented by exp. 
+void countSort(int arr[], int n, int exp) 
+{ 
+    int output[n]; // output array 
+    int i, count[10] = { 0 }; 
+  
+    // Store count of occurrences in count[] 
+    for (i = 0; i < n; i++) 
+        count[(arr[i] / exp) % 10]++; 
+  
+    // Change count[i] so that count[i] now contains actual 
+    //  position of this digit in output[] 
+    for (i = 1; i < 10; i++) 
+        count[i] += count[i - 1]; 
+  
+    // Build the output array 
+    for (i = n - 1; i >= 0; i--) { 
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i]; 
+        count[(arr[i] / exp) % 10]--; 
+    } 
+  
+    // Copy the output array to arr[], so that arr[] now 
+    // contains sorted numbers according to current digit 
+    for (i = 0; i < n; i++) 
+        arr[i] = output[i]; 
+} 
+  
+// The main function to that sorts arr[] of size n using 
+// Radix Sort 
+void radixsort(int arr[], int n) 
+{ 
+    // Find the maximum number to know number of digits 
+    int m = getMax(arr, n); 
+  
+    // Do counting sort for every digit. Note that instead 
+    // of passing digit number, exp is passed. exp is 10^i 
+    // where i is current digit number 
+    for (int exp = 1; m / exp > 0; exp *= 10) 
+        countSort(arr, n, exp); 
+} 
 
 void display(int array[], int n)
 {
