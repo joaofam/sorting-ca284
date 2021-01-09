9defc518db1263e428a2a17a1f911c0b14ca6008
Joao Pereira <joao.pereira2@mail.dcu.ie>
2021-01-09 21:35:02 +0000

    Selection of algorithims added

--- src/final_sort.c
+++ src/final_sort.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include "sorting_algorithims.h"
 #include "read_write_file.h"
+
+int selection_of_algorithims();
 
 int main()
 {
@@ -10,6 +13,18 @@ int main()
     fscanf(file, "%d", &n);
     array = file_read();
 
+
     file_write(array, n);
     return 0;
+}
+
+int selection_of_algorithims()
+{
+    int sort;        
+    printf("Select algorithim of choice:\n"
+            " - 1 - Selection Sort\n"
+            " - 2 - Insertion Sort\n"
+            " - 3 - Radix Sort\n");
+    scanf("%d", &sort);
+    return sort;
 }
