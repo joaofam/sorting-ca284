6c81552627127e5a9ad3d8422f9366d1a5df99dc
Joao Pereira <joao.pereira2@mail.dcu.ie>
2021-01-10 03:03:13 +0000

    datasets added

--- src/final_sort.c
+++ src/final_sort.c
@@ -4,6 +4,8 @@
 #include "sorting_algorithims.h"
 #include "read_write_file.h"
 
+int selection_of_dataset();
+int sort_dataset(int datasort, int * array, int n);
 
 int selection_of_algorithims();
 void sort_choice(int sort, int *array, int n);
@@ -17,11 +19,13 @@ int main()
     fscanf(file, "%d", &n);
     array = file_read();
 
+    int datasort = selection_of_dataset();
     int sort = selection_of_algorithims();
     
     clock_t start = clock();
     sort_choice(sort, array, n);
     clock_t end = clock();
+    sort_dataset(datasort, array, n);
 
     double execution_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
 
@@ -30,7 +34,32 @@ int main()
     file_write(array, n);
     return 0;
 }
+int selection_of_dataset()
+{
+    int datasort;
+    printf("Select dataset of choice:\n"
+          " - 1 - Ascending Sort\n"
+          " - 2 - Descending Sort\n"
+          " - 3 - Random Sort\n");
+    scanf("%d", &datasort);
+    return datasort;
+}
 
+
+int sort_dataset(int datasort, int * array, int n)
+{
+    if(datasort == 1)
+    qsort(array, n, sizeof(int), compare_ints);
+   else if(datasort == 2)
+    qsort(array, n, sizeof(int), descending_ints);
+  else if(datasort == 3)
+    return 0;
+  else{
+    printf("Not a valid number for dataset selection.\n");
+		exit(1);
+  }
+
+}
 int selection_of_algorithims()
 {
     int sort;        
@@ -55,6 +84,3 @@ void sort_choice(int sort, int *array, int n)
 		exit(1);
 	}
 }
-
-
-/* https://www.techiedelight.com/find-execution-time-c-program/ */
